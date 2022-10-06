4db05a888162148b9a2ff48ea090312ce8264947
fabioKesyn <fabio@thefullstackgroup.com>
2022-10-06 11:20:47 +0100

    lint fix

--- src/getConfig.ts
+++ src/getConfig.ts
@@ -1,36 +1,36 @@
 const getConfig = () => {
-    const cc = DataStudioApp.createCommunityConnector();
+  const cc = DataStudioApp.createCommunityConnector();
 
-    const config = cc.getConfig();
+  const config = cc.getConfig();
 
-    // Add name input
-    config
-        .newTextInput()
-        .setId('name')
-        .setName('Name')
-        .setHelpText('Your name for registering with the API')
-        .setAllowOverride(false);
+  // Add name input
+  config
+      .newTextInput()
+      .setId('name')
+      .setName('Name')
+      .setHelpText('Your name for registering with the API')
+      .setAllowOverride(false);
 
-    // Add email input
-    config
-        .newTextInput()
-        .setId('email')
-        .setName('Email')
-        .setHelpText('Your email for registering with the API')
-        .setAllowOverride(false);
+  // Add email input
+  config
+      .newTextInput()
+      .setId('email')
+      .setName('Email')
+      .setHelpText('Your email for registering with the API')
+      .setAllowOverride(false);
 
-    // Add an input for specifying a maximum number of posts to retrieve
-    config
-        .newSelectSingle()
-        .setId('postLimit')
-        .setName('Post limit')
-        .setHelpText(
-            'Maximum number of posts that will be fetched from the API'
-        )
-        .addOption(config.newOptionBuilder().setValue('10').setLabel('10'))
-        .addOption(config.newOptionBuilder().setValue('100').setLabel('100'))
-        .addOption(config.newOptionBuilder().setValue('1000').setLabel('1000'))
-        .setAllowOverride(false);
+  // Add an input for specifying a maximum number of posts to retrieve
+  config
+      .newSelectSingle()
+      .setId('postLimit')
+      .setName('Post limit')
+      .setHelpText(
+          'Maximum number of posts that will be fetched from the API',
+      )
+      .addOption(config.newOptionBuilder().setValue('10').setLabel('10'))
+      .addOption(config.newOptionBuilder().setValue('100').setLabel('100'))
+      .addOption(config.newOptionBuilder().setValue('1000').setLabel('1000'))
+      .setAllowOverride(false);
 
-    return config.build();
+  return config.build();
 };
--- src/getData.ts
+++ src/getData.ts
@@ -14,114 +14,113 @@ interface PostsData {
 }
 type Fields = GoogleAppsScript.Data_Studio.Fields;
 const getFields = (): Fields => {
-    const fields = cc.getFields();
-    const types = cc.FieldType;
+  const fields = cc.getFields();
+  const types = cc.FieldType;
 
-    fields
-        .newDimension()
-        .setId('userName')
-        .setName('Username')
-        .setType(types.TEXT);
+  fields
+      .newDimension()
+      .setId('userName')
+      .setName('Username')
+      .setType(types.TEXT);
 
-    fields
-        .newDimension()
-        .setId('postLength')
-        .setName('Post Length')
-        .setType(types.NUMBER);
-    return fields;
+  fields
+      .newDimension()
+      .setId('postLength')
+      .setName('Post Length')
+      .setType(types.NUMBER);
+  return fields;
 };
 
 const getData = (request: GetDataRequest): GetDataResponse => {
-    try {
+  try {
+    const requestedFields = getFields().forIds(
+        request.fields.map(({name}) => name),
+    );
 
-        const requestedFields = getFields().forIds(
-            request.fields.map(({ name }) => name)
-        );
+    const limit = request.configParams.postLimit || 100;
+    const properties = PropertiesService.getUserProperties();
+    const slToken = properties.getProperty('dscc.slToken');
 
-        const limit = request.configParams.postLimit || 100;
-        const properties = PropertiesService.getUserProperties();
-        const slToken = properties.getProperty('dscc.slToken');
-
-        if (slToken) {
-            const posts = fetchPostsData(slToken, limit);
-            const rows = toGetDataRows(posts, requestedFields);
-            return {
-                schema: requestedFields.build(),
-                rows: rows
-            };
-        } else {
-            userError('no valid slToken');
-        }
-    } catch (e) {
-        Logger.log('Error fetching data from API. Exception details: ' + e);
+    if (slToken) {
+      const posts = fetchPostsData(slToken, limit);
+      const rows = toGetDataRows(posts, requestedFields);
+      return {
+        schema: requestedFields.build(),
+        rows: rows,
+      };
+    } else {
+      userError('no valid slToken');
     }
-
+  } catch (e) {
+    Logger.log('Error fetching data from API. Exception details: ' + e);
+  }
 };
 const userError = (message: string) => {
-    cc.newUserError().setText(message).throwException();
+  cc.newUserError().setText(message).throwException();
 };
 const toGetDataRows = (
     response: PostsData[],
-    requestedFields: Fields
+    requestedFields: Fields,
 ): GetDataRows => {
-    const data: GetDataRows = [];
-    response.forEach((post: PostsData) => {
-        const row: GetDataRowValue[] = requestedFields
-            .asArray()
-            .map((requestedField) => {
-                switch (requestedField.getId()) {
-                    case 'userName':
-                        return post.from_name || '';
-                    case 'postLength':
-                        return (post.message || '').length;
-                    default:
-                        return '';
-                }
-            });
-        data.push({ values: row });
-    });
-    return data;
+  const data: GetDataRows = [];
+  response.forEach((post: PostsData) => {
+    const row: GetDataRowValue[] = requestedFields
+        .asArray()
+        .map((requestedField) => {
+          switch (requestedField.getId()) {
+            case 'userName':
+              return post.from_name || '';
+            case 'postLength':
+              return (post.message || '').length;
+            default:
+              return '';
+          }
+        });
+    data.push({values: row});
+  });
+  return data;
 };
 const fetchPostsData = (
-    sl_token: string,
+    slToken: string,
     limit: number,
 ): PostsData[] => {
-    let page = 1;
+  let page = 1;
 
-    let postsArray = doAPICall(sl_token, page).posts;
-    while (postsArray.length < limit) {
-        page++;
-        let extraCall = doAPICall(sl_token, page);
-        if (page > extraCall.page) {
-            break;
-        }
-        postsArray = postsArray.concat(extraCall.posts);
+  let postsArray = doAPICall(slToken, page).posts;
+  while (postsArray.length < limit) {
+    page++;
+    const extraCall = doAPICall(slToken, page);
+    // if no more pages returned from API, just stop the calls
+    if (page > extraCall.page) {
+      break;
     }
-    //if the API call returns more rows than the limit in the last call
-    postsArray.length = limit;
-    return postsArray;
+    postsArray = postsArray.concat(extraCall.posts);
+  }
+  // if the API call returns more rows than the limit in the last call
+  postsArray.length = limit;
+  return postsArray;
 };
 
 const doAPICall = (
-    sl_token: string,
-    page: number
+    slToken: string,
+    page: number,
 ): apiData => {
-    const url = `https://api.supermetrics.com/assignment/posts?sl_token=${sl_token}&page=${page}`;
-    const response = UrlFetchApp.fetch(url);
-    const statusCode = response.getResponseCode();
+  const url = `https://api.supermetrics.com/assignment/posts?sl_token=${slToken}&page=${page}`;
+  const response = UrlFetchApp.fetch(url);
+  const statusCode = response.getResponseCode();
 
-    if (statusCode !== 200) {
-        Logger.log('An exception occurred getting the posts:');
-        Logger.log(statusCode);
-        Logger.log(response.getAllHeaders());
-        Logger.log(response.getContentText());
-        userError(
-            `The API replied with an unsuccessful status code of ${statusCode}`
-        );
+  if (statusCode !== 200) {
+    Logger.log('An exception occurred getting the posts:');
+    Logger.log(statusCode);
+    Logger.log(response.getAllHeaders());
+    Logger.log(response.getContentText());
+    userError(
+        `The API replied with an unsuccessful status code of ${statusCode}`,
+    );
 
-        return;
-    }
-    //resetAuth();
-    const { data = {} } = JSON.parse(response.getContentText()) || {};
-    return data;
+    return;
+  }
+  // resetAuth();
+  const {data = {}} = JSON.parse(response.getContentText()) || {};
+  return data;
 };
